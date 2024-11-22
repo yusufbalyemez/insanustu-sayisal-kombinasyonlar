15f15ff97689ae810b673230a5051eaf592bc2f9
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-11-22 10:35:14 +0300

    Navbar updated

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import logo from "../images/logo.png";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { FaExchangeAlt } from "react-icons/fa";
+
 import {
   Button,
   Checkbox,
@@ -34,14 +36,20 @@ const Navbar = () => {
   }, []);
 
   const toggleDrawer = (open) => (event) => {
-    if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
       return;
     }
     setIsDrawerOpen(open);
   };
 
   const toggleDrawer2 = (open) => (event) => {
-    if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
       return;
     }
     setIsLeftDrawerOpen(open);
@@ -66,7 +74,8 @@ const Navbar = () => {
 
   const handleBasmalaCheckboxChange = (index) => {
     const updatedTempList = [...tempQuranList];
-    updatedTempList[index].startsWithBasmala = !updatedTempList[index].startsWithBasmala;
+    updatedTempList[index].startsWithBasmala =
+      !updatedTempList[index].startsWithBasmala;
     setTempQuranList(updatedTempList);
   };
 
@@ -115,23 +124,27 @@ const Navbar = () => {
           onKeyDown={toggleDrawer(false)}
           sx={{ color: "white" }}
         >
-          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((issue, index) => (
-            <ListItem
-              key={index}
-              component={Link}
-              to={issue === 0 ? "/" : `/${issue}`}
-              onClick={() => handleListItemClick(issue)}
-              sx={{
-                bgcolor: selectedItem === issue ? "black" : "inherit",
-                "&:hover": {
-                  bgcolor: "black",
-                },
-              }}
-            >
-              <AiOutlineNumber className="mr-1" />
-              <ListItemText primary={issue === 0 ? "Home" : `Issue ${issue}`} />
-            </ListItem>
-          ))}
+          {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(
+            (issue, index) => (
+              <ListItem
+                key={index}
+                component={Link}
+                to={issue === 0 ? "/" : `/${issue}`}
+                onClick={() => handleListItemClick(issue)}
+                sx={{
+                  bgcolor: selectedItem === issue ? "black" : "inherit",
+                  "&:hover": {
+                    bgcolor: "black",
+                  },
+                }}
+              >
+                <AiOutlineNumber className="mr-1" />
+                <ListItemText
+                  primary={issue === 0 ? "Home" : `Issue ${issue}`}
+                />
+              </ListItem>
+            )
+          )}
         </List>
       </Drawer>
 
@@ -141,7 +154,7 @@ const Navbar = () => {
         open={isLeftDrawerOpen}
         onClose={toggleDrawer2(false)}
         PaperProps={{
-          sx: { width: 400, bgcolor: "#374151", padding: "5px" },
+          sx: { width: 425, bgcolor: "#374151", padding: "5px" },
         }}
       >
         <div className="absolute top-2 right-5 border px-2 border-yellow-400 rounded-md">
@@ -154,7 +167,19 @@ const Navbar = () => {
         </div>
 
         <div className="flex flex-col items-center justify-center text-center text-white">
-          <span className="my-2 text-blue-300 font-bold">SUPERHUMAN NUMBERS</span>
+          <span className="my-2 text-blue-300 font-bold">
+            SUPERHUMAN NUMBERS
+          </span>
+        </div>
+        <div className="flex flex-col gap-2 mt-2">
+          <Button
+            type="submit"
+            variant="contained"
+            color="success"
+            onClick={handleDefaultQuranList}
+          >
+            Varsayılana Ayarla
+          </Button>
         </div>
         <table className="text-white text-center">
           <thead>
@@ -164,6 +189,7 @@ const Navbar = () => {
               <th>Ayetler</th>
               <th>Huruf</th>
               <th>Besmele</th>
+              <th>Değiştir</th>
             </tr>
           </thead>
           <tbody>
@@ -191,18 +217,20 @@ const Navbar = () => {
                     onChange={() => handleBasmalaCheckboxChange(index)}
                   />
                 </td>
+                <td>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    onClick={handleUpdateQuranList}
+                  >
+                    <FaExchangeAlt />
+                  </Button>
+                </td>
               </tr>
             ))}
           </tbody>
         </table>
         <div className="flex flex-col gap-2 mt-2">
-          <Button
-            type="submit"
-            variant="contained"
-            onClick={handleUpdateQuranList}
-          >
-            Değiştir
-          </Button>
           <Button
             type="submit"
             variant="contained"
