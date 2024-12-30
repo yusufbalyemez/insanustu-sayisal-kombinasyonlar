60c77f5f6ceb1536cc38b4921fb04458ed3f1525
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-12-30 11:08:27 +0300

    israiloğullarından bir şahit bilgisi eklendi

---
+++ app/src/Informations/IsrailogullariSahit.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const IsrailogullariSahit = () => {
+  return (
+    <div className="border border-gray-300 p-5 rounded-lg bg-white lg:w-3/4 mx-auto my-5">
+      <h2 className="text-center text-xl font-bold">İsrailoğullarından Bir Şahit [46:10]</h2>
+      <div className="border border-red-800 p-3 italic my-4">
+        <p>
+          İlan et: 
+          <q>Ya o Tanrı’dan ise ve siz de onu inkâr ettiyseniz? İsrailoğullarından bir şahit benzer bir olguya tanıklık etti ve iman etti, oysa sizler iman etmeyecek kadar kibirlendiniz. Tanrı kötülere rehberlik etmez.</q>
+          [46:10]
+        </p>
+      </div>
+      <p className="my-2">
+        Aşağıdaki alıntı, <strong>STUDIES IN JEWISH MYSTICISM</strong>’den (YAHUDİ MİSTİSİZMDE ÇALIŞMALAR) alınmıştır
+        (Association for Jewish Studies, Cambridge, Mass., Joseph Dan & Frank Talmage, eds., Page 88, 1982). Bu alıntı,
+        Takva Ehli olan Haham Juda’nın çalışmasına atıf yapmaktadır (MS 12. Yüzyıl):
+      </p>
+      <p className="my-2">
+        Fransa’daki halk [Yahudiler], [sabah ibadetine] şu sözleri eklemeyi gelenek haline getirdiler: 
+        <q>’Ashrei temimei derekh [kutsananlar doğru yolu yürüyenlerdir]</q> ve Kutsanmış bir hafızaya sahip olan bizim takva ehli Haham, onların tamamen 
+        ve düpedüz hatalı olduğunu yazdı. Bunların hepsi büyük sahteciliktir çünkü [sabah ibadetinin bu bölümünde] Kutsal İsim
+        sadece on dokuz kez anılır... ve benzer biçimde ’Elohim kelimesini Ve-elle shemut... pasajında on dokuz kez
+        bulursunuz. Benzer şekilde, İsrail’in “oğullar” diye on dokuz kez çağrıldığını görürsünüz ve daha birçok başka
+        örnekler mevcuttur. Tüm bu on dokuz grupları karmaşık bir şekilde içe geçmelidir ve sekizden fazla ciltte yer alan
+        birçok gizemler ve özel anlamlar içermektedir. Dahası, bu bölümde 152 (19x8) kelime vardır.
+      </p>
+      <h3 className="text-center text-lg font-bold mt-5">
+        TÜM ÖVGÜ, KÂİNATIN RABBİ OLAN TANRI'YADIR.
+      </h3>
+    </div>
+  );
+};
+
+export default IsrailogullariSahit;
--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import SurasList from '../components/SurasList'
+import IsrailogullariSahit from '../Informations/IsrailogullariSahit'
 
 const HomePage = () => {
   return (
     <div className='flex flex-col md:justify-center md:items-center p-3'>
         <SurasList/>
         <p>Home Page</p>
+        <IsrailogullariSahit/>
     </div>
   )
 }
