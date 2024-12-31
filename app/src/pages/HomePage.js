8b820711f6439f0bddad09c139a67714636963d7
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2024-12-31 12:40:40 +0300

    Intro added

--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import SurasList from '../components/SurasList'
 import IsrailogullariSahit from '../Informations/IsrailogullariSahit'
+import Intro from '../components/Intro'
 
 const HomePage = () => {
   return (
     <div className='flex flex-col md:justify-center md:items-center p-3'>
         <SurasList/>
-        <p>Home Page</p>
+        <Intro/>
         <IsrailogullariSahit/>
     </div>
   )
