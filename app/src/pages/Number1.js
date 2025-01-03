001709af0c588d3e94e02a887113608b1822a9c6
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2025-01-03 18:44:50 +0300

    Number1 and number2 infos added

--- app/src/pages/Number1.js
+++ app/src/pages/Number1.js
@@ -15,6 +15,7 @@ import ShowButtonToggle from "../components/ShowButtonToggle";
 import ResultDisplay from "../components/ResultDisplay";
 import CopyAndSelectButtons from "../components/CopyAndSelectButtons";
 import { useDifferentRefs } from "../context/DifferentRefsContext";
+import Number1Info from "../Informations/Number1Info";
 
 const Number1 = () => {
   const { quranList } = useQuran(); //Jsondaki Orjinal Kuran listesini bu değişkene aktarır.
@@ -191,50 +192,7 @@ const Number1 = () => {
           })}
         </div>
       ) : (
-        <div
-          className="text-white flex flex-col items-center gap-10 break-words
-         border border-gray-300 p-4 mb-5 w-full md:w-10/12 bg-gradient-to-l
-          from-gray-700 to-gray-800 rounded-lg text-xl overflow-y-auto max-h-[60vh]
-           hover:ring-4 hover:ring-yellow-400 transition-all"
-        >
-          <p className="text-yellow-400 flex items-center gap-2">
-            <SiMiraheze />
-            Birinci Sayı
-          </p>
-          <p>
-            Kuran’daki her bir ayetin numarasını yazalım, önüne de her bir sure
-            için bu suredeki ayet sayısını yazalım. Böylelikle, yedi ayetten
-            oluşan Sure 1, 7 1234567 sayısı ile temsil edilecektir. Burada
-            yaptığımız şey, ayet sayılarını yan yana yazarak uzun sayılar
-            oluşturmaktır. Sure 2’yi temsil eden sayıyı bulmak için bu suredeki
-            ayet sayısı olan 286, ardından her bir ayetin numarasını yan yana
-            yazılmış şekilde yazarsınız. Böylelikle, Sure 2’yi temsil eden sayı
-            şu şekilde gözükür: 286 12345…284285286. İlk iki sureyi temsil eden
-            iki sayı şunlardır:
-          </p>
-          <p className="text-green-300">
-            7 1 2 3 4 5 6 7 & 286 1 2 3 4 5….284 285 286.
-          </p>
-          <p>
-            İlk iki sureyi temsil eden bir sayı oluşturmak için bu iki sayıyı
-            bir araya getirirsek, şu sayıyı alırız:
-          </p>
-          <p className="text-green-300">
-            7 1 2 3 4 5 6 7 286 1 2 3 4 5.....284 285 286.
-          </p>
-          <p>
-            Bu işlem, Kuran’daki her bir ayet yazılıncaya kadar devam ettirilir,
-            böylece Kuran’daki her ayetin numarasını içine alan çok uzun bir
-            sayı oluşur. Tüm Kuran’ı temsil eden sayı 19’un bir katıdır & 12692
-            basamaklıdır ki bu da 19’un bir katıdır.
-          </p>
-          <p>
-            Bu çok uzun sayı 12692 (19x668) basmaktan oluşur ve Kuran’daki her
-            bir ayeti içermektedir. Her bir suredeki ayet sayısı, ayetlerinin
-            önünde yer almaktadır. Çok uzun sayıları bölen özel bir bilgisayar
-            programı bu sayının 19’un katı olduğunu gösterdi.
-          </p>
-        </div>
+        <Number1Info/>
       )}
     </div>
   );
