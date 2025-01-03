b51d1972a1622bcc8aa492426cdd6e90099dde93
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2025-01-03 19:04:04 +0300

    number4 and 5 info translated

--- app/src/Informations/Number4Info.js
+++ app/src/Informations/Number4Info.js
@@ -2,26 +2,21 @@ import React from "react";
 import BilgiMetniniGosterenComponent from "../components/BilgiMetniniGosterenComponent";
 import { SiMiraheze } from "react-icons/si";
 
+import { useLanguage } from "../context/LanguageContext";
+
 const Number4Info = () => {
+  const {translations} = useLanguage();
   return (
     <BilgiMetniniGosterenComponent>
       <p className="text-yellow-400 flex items-center gap-2">
         <SiMiraheze />
-        Dördüncü Sayı
+        {translations.number4Info.title}
       </p>
       <p>
-        Her suredeki toplam ayet sayısını sureden sonra koymak yerine, bunu
-        surenin önüne koyalım. Böylelikle, Sure 1’i temsil eden sayı 1234567 1 7
-        yerine şu şekilde gözükür: 7 1234567 1 ve Sure 2’yi temsil eden sayı
-        12345.....284 285 286 2 286 yerine şu şekilde gözükür: 286 12345.... 284
-        285 286 2. Tüm Kuran’ı temsil eden bu çok uzun sayı 19’un bir katıdır.
+        {translations.number4Info.text1}
       </p>
       <p>
-        Her bir suredeki ayetlerin toplam sayısını her bir ayetin numarası,
-        ardından sure numarası takip eder. Aşağıda gösterilen son 14 basamak,
-        son suredeki ayet sayısı (6), ardından altı ayetin numaraları (123456),
-        ardından surenin numarası (114), sonra Kuran’daki toplam numaralı ayet
-        sayısıdır. (12930 basamaktan oluşan) bu çok uzun sayı 19’un bir katıdır.
+      {translations.number4Info.text2}
       </p>
       <p className="text-green-300">
         7 1234567 1 286 12345…286 2…6 123456 114 6234
--- app/src/Informations/Number5Info.js
+++ app/src/Informations/Number5Info.js
@@ -2,24 +2,31 @@ import React from "react";
 import BilgiMetniniGosterenComponent from "../components/BilgiMetniniGosterenComponent";
 import { SiMiraheze } from "react-icons/si";
 
+import { useLanguage } from "../context/LanguageContext";
+
 const Number5Info = () => {
+  const { translations } = useLanguage();
   return (
     <BilgiMetniniGosterenComponent>
       <p className="text-yellow-400 flex items-center gap-2">
         <SiMiraheze />
-        Beşinci Sayı
+        {translations.number5Info.title}
       </p>
       <p>
-        Her suredeki her ayet numarasını ayet numaralarının toplamı takip eder. Bu uzun sayı 12836 basamaktan oluşur ve 19’un bir katıdır.
+      {translations.number5Info.text1}
       </p>
       <p>
-        Şimdi, her suredeki her ayetin numarasını, ardından her sure için ayet numaralarının toplamını yazalım. Sure 1, 7 ayetten oluşmaktadır ve ayet numaralarının toplamı 1 + 2 + 3 + 4 + 5 + 6 + 7 = 28’dir. Böylelikle, Sure 1’i temsil eden sayı şu şekilde gözükür: 1234567 28.
+      {translations.number5Info.text2}
       </p>
       <p>
-        Sure 2 için ayet numaralarının toplamı 41041’dir (1 + 2 + 3 + ... + 286). Böylelikle, Sure 2’yi temsil eden sayı şu şekilde gözükür: 12345…284 285 286 41041.
+      {translations.number5Info.text3}
       </p>
-      <p>6 ayetten oluşan son sureyi temsil eden sayı şu şekilde gözükür: 123456 21 çünkü 1 + 2 + 3 + 4 + 5 + 6 = 21.</p>
-      <p>Tüm Kuran’ı temsil eden bütün sayı 12836 basamaktan oluşmaktadır ve 19’un bir katıdır. Şu şekilde gözükür.</p>
+      <p>
+      {translations.number5Info.text4}
+      </p>
+      <p>
+      {translations.number5Info.text5}
+      </p>
       <p className="text-green-300">
         1234567 28 12345...284285286 41041...123456 21
       </p>
