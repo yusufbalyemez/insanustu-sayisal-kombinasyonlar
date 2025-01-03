7e1095c3975dec54a9df28f9d9e7805266dddaab
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2025-01-03 15:17:00 +0300

    Homepage has been translated to english

--- app/src/components/Intro.js
+++ app/src/components/Intro.js
@@ -1,107 +1,108 @@
 import resim1 from '../assets/images/resim1.jpg';
 import resim2 from '../assets/images/resim2.jpg';
+import { useLanguage } from "../context/LanguageContext";
 
 const Intro = () => {
+    const { translations } = useLanguage(); // Çeviri verilerini al
     return (
         <div className="p-4 space-y-4 bg-gray-100 lg:w-3/4 lg:mx-auto my-5 mt-20">
-            <h2 className="text-center text-lg font-semibold">Tanrı’nın Sözü ile Oynamak</h2>
+            <h2 className="text-center text-lg font-semibold">{translations.homepage.title}</h2>
 
             <div className="border p-4 bg-gray-50 rounded">
                 <p>
-                    İnsanüstü matematiksel bir sistem, Kuran’ın her tarafına yayılır ve onun içindeki her bir öğeyi korumaya ve gerçekliğini kanıtlamaya hizmet eder. Peygamberin ölümünden on dokuz yıl sonra, bazı kâtipler, Medine’de vahyolunan son sure olan Sure 9’un sonuna iki sahte ayet sokuşturdular. Bu Ek’te sunulan deliller, tartışmasız bir şekilde bu insan sokuşturmalarını ortadan kaldırır, Kuran’ın bozulmamış saflığına geri döndürür ve Kuran’ın matematiksel kodunun büyük bir işlevini yani Kuran’ın en ufak bir sokuşturmaya karşı korumayı sergiler. Nitekim kod SADECE 9:128-129 sahte sokuşturmalarını reddeder.
+                {translations.homepage.text1}
                 </p>
             </div>
 
             <div className="border p-4 bg-gray-50 rounded">
-                <p><strong>[15:9]</strong></p>
-                <p>Şüphesiz, bu kutsal yazıyı biz vahyettik ve şüphesiz, onu biz koruyacağız.</p>
+                <p>{translations.homepage.text2}</p>
             </div>
 
             <div className="border p-4 bg-white rounded shadow">
                 <p>
-                    Kuran, Tanrı’nın Son Ahitidir. Onu mükemmel bir şekilde korumak için verilen ilahi söz bu nedenledir. Hem ilahi yazarlık, hem de Kuran’ın mükemmel korunması hususunda bizleri emin kılmak için, Yüce yazar, Kuran’ı matematiksel olarak oluşturmuştur. Ek 1’de fiziksel delil ile ispatlandığı gibi, böylesi bir matematiksel yapı insan yeteneklerinin çok ötesindedir. Tanrı’nın Son Ahitinin en ufak bozuluşu, göze batan bir uyumsuzlukla dikkat çekmeye mukadderdir. Sadece 1—bir sure, bir ayet, bir kelime, hatta bir harf—ile olan sapma anında ifşa olur.
+                {translations.homepage.text3}
                 </p>
             </div>
 
             <div className="border p-4 bg-white rounded shadow">
                 <p>
-                    Muhammed Peygamber’in ölümünden on dokuz yıl sonra, Halife Osman’ın halifeliği döneminde, yeni Müslüman topraklara gönderilmek üzere Kuran’ın birkaç kopyasını yapmak için bir yazıcı komitesi atandı. Kopyaların, Muhammed’in eliyle yazılmış olan orijinal Kuran’dan yapılması gerekiyordu (Ek 28).
+                {translations.homepage.text4}
                 </p>
             </div>
 
             <div className="border p-4 bg-white rounded shadow">
                 <p>
-                    Bu komite, Osman bin Affan, Ali bin Ebu Talip, Zeyd bin Sabit, Ubey bin Kab, Abdullah bin Zübeyr, Saîd bin Âs ve Abdurrahman bin el-Hâris bin Hişâm tarafından gözetilip denetlenmekteydi. Peygamber, elbette ki Kuran’ı kronolojik vahiy sırasına göre (Ek 23), her parçayı uygun pozisyonuna yerleştirmek için gerekli talimatlarla birlikte yazmıştı. Medine’de vahyolunan son sure, Sure 9’du. Sadece çok kısa bir sure olan Sure 110, Sure 9’dan sonra Mina’da vahyedildi.
+                {translations.homepage.text5}
                 </p>
             </div>
 
             <div className="border p-4 bg-white rounded shadow">
                 <p>
-                    Yazıcılar komitesi nihayet Sure 9’a geldiler ve onu doğru yerine koydular. Yazıcılardan biri, Peygamberi onurlandırmak için birkaç ayet eklemeyi önerdi. Yazıcılardan birçoğu kabul etti. Ali ise öfkelendi. Tanrı’nın, son peygamberinin eli ile kâğıda dökülmüş olan sözünün asla değiştirilmemesi gerektiğini şiddetle savundu.
+                {translations.homepage.text6}
                 </p>
             </div>
             <div className="border p-4 bg-gray-50 rounded">
-                <p>Ali’nin protestosu birçok referansta belgelenmiştir ancak ben burada Celalettin Suyuti tarafından yazılmış EL-İTKAN Fİ ULUMİL KURAN, Al-Azhareyyah Press, Kahire, Mısır, Hicri 1318, Sayfa 59 [bkz. İlave 1] klasik referansını alıntılayıp aslına uygun kopyalıyorum.</p>
+                <p>{translations.homepage.text7}</p>
                 <img src={resim1} alt="resim1" className="w-full lg:w-1/2 mx-auto my-4 border" />
-                <p>Tercümesi: Ali’ye soruldu: “Neden evde duruyorsun?” Dedi ki “Kuran’a bir şeyler eklendi ve Kuran eski haline getirilinceye kadar namaz hariç gündelik giysilerimi asla giymeyeceğime söz verdim.”</p>
+                <p>{translations.homepage.text8}</p>
             </div>
 
             <div className="border p-4 bg-white rounded shadow">
-                <p>Bu suçun korkunç boyutları, sonuçlara baktığımız an fark edilebilir:</p>
-                <p>(1) Osman suikasta uğradı ve Ali dördüncü Halife olarak başa geçirildi.</p>
-                <p>(2) Bir tarafta yeni Halife ve onun destekçileri ile diğer tarafta Kuran’ın Muhammed-perest tahrifçileri arasında 50 yıl süren bir savaş patlak verdi.</p>
-                <p>(3) Ali şehit edildi ve nihayetinde onun ailesi, yani Muhammed peygamberin ailesi, bazı kadın ve çocuklar hariç öldürüldü.</p>
-                <p>(4) Felaket, Ali’nin oğlu Hüseyin ve ailesinin katledildiği kötü namlı Kerbela Savaşı ile sonuçlandı.</p>
-                <p>(5) Müslümanlar 1400 yıl boyunca saf, değiştirilmemiş Kuran’dan mahrum edildiler.</p>
+                <p>{translations.homepage.text9}</p>
+                <p>{translations.homepage.text10}</p>
+                <p>{translations.homepage.text11}</p>
+                <p>{translations.homepage.text12}</p>
+                <p>{translations.homepage.text13}</p>
+                <p>{translations.homepage.text14}</p>
             </div>
 
             <div className="border p-4 bg-white rounded shadow">
-                <p>Kuran’ı tahrif edenler sonunda savaşı kazandı ve bize gelen “resmi” tarih, galiplerin bakış açısını temsil ediyordu. Tanrı’nın düşmanları için görünürdeki bu galibiyet, elbette ki Tanrı’nın iradesine göreydi. Peygamberin ölümünden sadece yirmi yıl sonra, Mekke’nin fethinde (MS 632) Peygamber tarafından mağlup edilen puta tapanlar, putperestliğe geri döndüler. İronik bir şekilde, bu kez putları Peygamberin kendisiydi. Bu gibi puta tapanlar belli ki saf Kuran’a sahip olmayı hak etmiyorlardı. Kuran’ı eski haline getirmeye çalışan gerçek imanlıların kutlu şehadeti ve Tanrı sözünü tahrif edenlerin görünüşteki galibiyeti bunun sonucudur.</p>
+                <p>{translations.homepage.text15}</p>
             </div>
 
             <div className="border p-4 bg-white rounded shadow">
                 <p>
-                    Bu uzun ve felaketli savaşın ardından ilk barış zamanı idarecisi Mervan Bin El-Hakem’di (MS 684/Hicri 65’te öldü). Yaptığı ilk icraatlardan biri de “YENİ anlaşmazlıklara neden olacağından korkarak” Peygamberin kendi eliyle çok titizlikle yazılmış olan orijinal Kuran’ı yok etmekti [bkz. ULUMUL KURAN, Ahmed von Denffer, Islamic Foundation, Leicester, United Kingdom, 1983, Sayfa 56]. Akıllı bir kişinin şu soruyu sorması gerekir: “Eğer orijinal Kuran, o sırada dolaşımda olan Kuran’la aynı ise, neden Mervan Bin El-Hakem onu imha etmek zorundaydı?!
+                {translations.homepage.text16}
                 </p>
             </div>
 
             <div className="border p-4 bg-white rounded shadow">
                 <p>
-                    En eski İslami referansları inceledikten sonra, 9:128-129 sahte sokuşturmalarının her zaman şüpheli olduğunu fark ediyoruz. Örneğin, Buhari’nin ünlü Hadis’inde ve Suyuti’nin ünlü İtkan’ında Kuran’daki her ayetin çok sayıda tanık tarafından teyit edildiğini okuyoruz, “Sure 9’un 128 ve 129. Ayetleri hariç; onlar sadece Huzeyme bin Sabit El-Ensari’de vardı.” Bazı insanlar bu uygunsuz istisnayı sorguladığında, birileri şunu söyleyen bir Hadis ortaya attılar, “Huzeyme’nin tanıklığı iki adamın tanıklığına eşittir!!!
+                {translations.homepage.text17}
                 </p>
             </div>
 
             <div className="border p-4 bg-gray-50 rounded">
-                <p>Tuhaf bir şekilde, 9:128-129 sokuşturmaları geleneksel Kuran baskılarında “Mekkî” olarak sınıflandırılmaktadır [bkz. İlave 2].</p>
+                <p>{translations.homepage.text18}</p>
             </div>
 
             <div className="border p-4 bg-gray-50 rounded">
                 <img src={resim2} alt="resim2" className="w-full lg:w-1/2 mx-auto my-4 border" />
-                <p>Standart bir Kuran’dan Sure 9’un Başlık Şekli, bu surenin Medenî olduğunu gösteriyor, “son iki ayet hariç; onlar Mekkî’dir”!!! [İlave 2 ]</p>
+                <p>{translations.homepage.text19}</p>
             </div>
 
             <div className="border p-4 bg-white rounded shadow">
                 <p>
-                    Son dönem ‘Medineli’ bir Müslüman olan Huzeyme’de bu ‘Mekkî’ ayetler nasıl bulunabildi?! Evrensel kabul Peygamberin Mekke’den Hicretinden sonraki tüm vahiyleri “Medeni” olarak sınıflandırmışken, nasıl olur da Medeni bir sure Mekkî ayetler içerebilir?!! Bu tutarsızlıklara, ayrıca 9:128-129 Ayetleri ile alakalı göze çarpan çelişkilere rağmen, hiç kimse onların gerçekliğini sorgulamaya cesaret edemedi. Bununla birlikte, Kuran’ın matematiksel kodunun 1974’te keşfedilmesi, Kuran’daki her unsurun gerçekliğinin kanıtlandığı yeni bir çağ açmıştır (Ek 1).
+                {translations.homepage.text20}
                 </p>
             </div>
 
             <div className="border p-4 bg-white rounded shadow">
-                <p>Anlaşıldığı üzere, iki sahte Ayet olan 9:128-129’un sokuşturulması şu sonuçları ortaya çıkardı:</p>
-                <p>(1) Kuran’ın matematiksel sisteminin asıl fonksiyonunu göstermek,</p>
-                <p>(2) başlı başına harika bir mucize ortaya koymak,</p>
-                <p>(3) gerçek imanlıları ikiyüzlülerden ayırmak (onlar geleneklere sarılırlar).</p>
+                <p>{translations.homepage.text21}</p>
+                <p>{translations.homepage.text22}</p>
+                <p>{translations.homepage.text23}</p>
+                <p>{translations.homepage.text24}</p>
             </div>
 
             <div className="border p-4 bg-gray-50 rounded">
-                <p>İki sahte ayetin tercümesi İlave 3’te gösterilmektedir:</p>
-                <p>“İçinizden size bir elçi gelmiş bulunuyor, ki size hiçbir sıkıntının ilişmesini istemez ve size düşkündür, imanlılara karşı şefkatli, merhametlidir. Eğer yüz çevirirlerse o zaman de ki, ‘Tanrı bana yeter, O’ndan başka tanrı yoktur. Ben O’na güveniyorum. O, büyük tahtın Rabbidir.’ “[İlave 3]</p>
+                <p>{translations.homepage.text25}</p>
+                <p>{translations.homepage.text26}</p>
             </div>
             <div className="border p-4 bg-gray-50 rounded">
-                <p><a href='https://kuransonahit.org/ek/24' className='pointer text-blue-500 hover:opacity-75 underline'>Bu konuda daha fazla fiziksel kanıt için Ek 24'e bakınız</a></p>
+                <p><a href='https://kuransonahit.org/ek/24' className='pointer text-blue-500 hover:opacity-75 underline'>{translations.homepage.text27}</a></p>
             </div>
             <div className="border p-4 bg-gray-50 rounded">
-               <p>İnsanüstü sayısal kompozisyonları incelemek için Sayılar menüsüne tıklayın.</p>
+               <p>{translations.homepage.text28}</p>
             </div>
 
         </div>
