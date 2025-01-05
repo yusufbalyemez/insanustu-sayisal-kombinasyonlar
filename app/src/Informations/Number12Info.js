60f6f61fd3fd2a9195f4f6cffbcdb721d365a3d6
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2025-01-05 11:42:52 +0300

    Sure 12 ye kadar 12 dahil tüm bilgiler orjinal çeviriyle düzeltildi

--- app/src/Informations/Number12Info.js
+++ app/src/Informations/Number12Info.js
@@ -5,8 +5,8 @@ import { useLanguage } from "../context/LanguageContext";
 
 
 const Number12Info = () => {
-    const { translations } = useLanguage();
-  
+  const { translations } = useLanguage();
+
   return (
     <BilgiMetniniGosterenComponent>
       <p className="text-yellow-400 flex items-center gap-2">
@@ -14,8 +14,11 @@ const Number12Info = () => {
         {translations.number12Info.title}
       </p>
       <p>
-      {translations.number12Info.text1}     
-       </p>
+        {translations.number12Info.text1}
+      </p>
+      <p>
+        {translations.number12Info.text2}
+      </p>
       <p className="text-green-300">
         114 6234 333410 1 1 2 3 4 5 6 7...114 1 2 3 4 5 6
       </p>
