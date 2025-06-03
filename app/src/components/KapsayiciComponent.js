805b4c6a45cada840d4b7a9100f75f94196d1a03
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2025-06-03 17:09:21 +0300

    kapsayici divdeki margin top değeri düzeltildi

--- app/src/components/KapsayiciComponent.js
+++ app/src/components/KapsayiciComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const KapsayiciComponent = ({children}) => {
   return (
-    <div className="flex flex-col justify-center items-center gap-2 mt-5">{children}</div>
+    <div className="flex flex-col justify-center items-center gap-2 mt-5 md:mt-10">{children}</div>
   )
 }
 
