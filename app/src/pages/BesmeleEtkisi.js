81aefb7d12f07acc6ba06c2834c56075931e84b5
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2025-06-10 23:31:05 +0300

    Besmele Etkisi düzenlendi

--- app/src/pages/BesmeleEtkisi.js
+++ app/src/pages/BesmeleEtkisi.js
@@ -14,6 +14,7 @@ import {
   sureNumarasiniHesaplamaDizisineEkle,
   suredekiToplamAyetSayisiniHesaplamaDizisineEkle,
   kurandakiToplamAyetSayisiniHesaplamaDizisineEkle,
+  handleAyatClick,
 } from "../components/Functions";
 import { useQuran } from "../context/quranListContext";
 import QURAN from "../assets/SurahInfo.json";
@@ -27,25 +28,21 @@ import SayiyiGosterenComponent from "../components/SayiyiGosterenComponent";
 import KapsayiciComponent from "../components/KapsayiciComponent";
 import BesmeleEtkisiInfo from "../Informations/BesmeleEtkisiInfo";
 
-
 const BesmeleEtkisi = () => {
-  const { quranList } = useQuran(); //Jsondaki Orjinal Kuran listesini bu değişkene aktarır.
-  const [orginQuranEmptyList, setOrginQuranEmptyList] = useState([]); //JSONdaki bilgileri büyük sayı hale getirmek için kullanılan boş dizi.
-  const [olusanDizi, setOlusanDizi] = useState([]); //Jsondaki ve tabloda değişiklik olursa oluşacak sayıyı oluşturan boş dizi
+  const { quranList } = useQuran();
+  const [orginQuranEmptyList, setOrginQuranEmptyList] = useState([]);
+  const [olusanDizi, setOlusanDizi] = useState([]);
   const [stringSayi, setStringSayi] = useState("");
   const [goster, setGoster] = useState(false);
   const [selectedSurahs, setSelectedSurahs] = useState([]);
   const [copyState, setCopyState] = useState(false);
+  const { differentRefs } = useDifferentRefs();
 
   const handleCopy = () => {
     navigator.clipboard
       .writeText(stringSayi)
-      .then(() => {
-        toast.success("Sayı Kopyalandı.");
-      })
-      .catch((err) => {
-        toast.error("Kopyalama başarısız.");
-      });
+      .then(() => toast.success("Sayı Kopyalandı."))
+      .catch(() => toast.error("Kopyalama başarısız."));
     setCopyState(true);
   };
 
@@ -54,84 +51,44 @@ const BesmeleEtkisi = () => {
     setGoster(!goster);
   };
 
-  // Context üzerinden referanslara erişim
-  const { differentRefs } = useDifferentRefs();
-
-  const buyukSayiyiOlustur = () => {
-    let bosDizi = [];
-    let orjinalBosDizi = [];
-    let stringBuyukSayi = "";
-
-    //Sayının Gösterildiği Kısım - Start
+  useEffect(() => {
+    const orjinalBosDizi = [];
     QURAN.forEach((sure) => {
       suredekiToplamAyetSayisiniYazdir(sure, orjinalBosDizi);
-      if (sure.startsWithBasmala){
+      if (sure.startsWithBasmala) {
         orjinalBosDizi.push({ durum: "besmele", sureNo: sure.surahNumber, sureAdi: sure.surahName, deger: 0 });
       }
       suredekiTumAyetSayilariniYazdir(sure, orjinalBosDizi);
       sureNumarasiniYazdir(sure, orjinalBosDizi);
     });
-
     kurandakiToplamAyetSayisiniYazdir(QURAN, orjinalBosDizi);
-    //Sayının Gösterildiği Kısım - End
-
-    //Hesaplanacak Büyük Sayının Oluşturulduğu kısım - Start
-    quranList.forEach((sure) => {
-      ({ bosDizi, stringBuyukSayi } =
-        suredekiToplamAyetSayisiniHesaplamaDizisineEkle(
-          sure,
-          bosDizi,
-          stringBuyukSayi
-        ));
-        if (sure.startsWithBasmala){
-          bosDizi.push({ durum: "besmele", sureNo: sure.surahNumber, sureAdi: sure.surahName, deger: 0 });
-          stringBuyukSayi += "0";
-        }
-      ({ bosDizi, stringBuyukSayi } =
-        suredekiTumAyetSayilariniHesaplamaDizisineEkle(
-          sure,
-          bosDizi,
-          stringBuyukSayi
-        ));
-      ({ bosDizi, stringBuyukSayi } = sureNumarasiniHesaplamaDizisineEkle(
-        sure,
-        bosDizi,
-        stringBuyukSayi
-      ));
-    });
-
-    ({ bosDizi, stringBuyukSayi } =
-      kurandakiToplamAyetSayisiniHesaplamaDizisineEkle(
-        quranList,
-        bosDizi,
-        stringBuyukSayi
-      ));
-
-    //Hesaplanacak Büyük Sayının Oluşturulduğu kısım - End
-
-    setOlusanDizi(bosDizi);
     setOrginQuranEmptyList(orjinalBosDizi);
-    setStringSayi(stringBuyukSayi);
-  };
+  }, []);
 
   useEffect(() => {
-    buyukSayiyiOlustur();
+    let bosDizi = [];
+    let stringBuyukSayi = "";
+    quranList.forEach((sure) => {
+      ({ bosDizi, stringBuyukSayi } = suredekiToplamAyetSayisiniHesaplamaDizisineEkle(sure, bosDizi, stringBuyukSayi));
+      if (sure.startsWithBasmala) {
+        bosDizi.push({ durum: "besmele", sureNo: sure.surahNumber, sureAdi: sure.surahName, deger: 0 });
+        stringBuyukSayi += "0";
+      }
+      ({ bosDizi, stringBuyukSayi } = suredekiTumAyetSayilariniHesaplamaDizisineEkle(sure, bosDizi, stringBuyukSayi));
+      ({ bosDizi, stringBuyukSayi } = sureNumarasiniHesaplamaDizisineEkle(sure, bosDizi, stringBuyukSayi));
+    });
+    ({ bosDizi, stringBuyukSayi } = kurandakiToplamAyetSayisiniHesaplamaDizisineEkle(quranList, bosDizi, stringBuyukSayi));
+    setOlusanDizi(bosDizi);
+    setStringSayi(stringBuyukSayi);
   }, [quranList]);
 
   return (
     <KapsayiciComponent>
-      {/* Sayfanın başlığını ayarlama */}
       <Helmet>
         <title>Besmele Etkisi</title>
       </Helmet>
-
-      {/* 19'a bölümünden kalanını ve basamak sayısını gösteren bileşen */}
       <ResultDisplay stringSayi={stringSayi} calculateMod19={calculateMod19} />
-
-      {/* Sayıyı yada Açıklama Metnini Gösteren Bileşen */}
       <ShowButtonToggle toggleGoster={toggleGoster} goster={goster} />
-
-      {/* Sayıyı kopyalayan ve seçimleri iptal eden buton bileşeni */}
       <CopyAndSelectButtons
         copyState={copyState}
         goster={goster}
@@ -142,64 +99,29 @@ const BesmeleEtkisi = () => {
         <SayiyiGosterenComponent>
           {olusanDizi.map((eleman, index) => {
             const isDiff = isDifferent(eleman, orginQuranEmptyList);
-
-            if (isDiff) {
-              // Farklı elemanları referans nesnesine kaydet
-              differentRefs.current[index] = null;
-            }
+            if (isDiff) differentRefs.current[index] = null;
             return (
               <span
                 key={index}
                 ref={(el) => {
-                  if (isDiff) {
-                    differentRefs.current[index] = el; // Sadece farklı elemanları ekliyoruz
-                  } else {
-                    delete differentRefs.current[index]; // Farklı olmayanları temizliyoruz
-                  }
+                  if (isDiff) differentRefs.current[index] = el;
+                  else delete differentRefs.current[index];
                 }}
+      
                 onClick={() => {
-                  if (eleman.durum === "toplam-ayet-sayisi") {
-                    handleTotalAyatClick(eleman.deger);
-                  } 
-                   else if (eleman.durum === "ayet-sayisi") {
-                    handleTotalClick(
-                      eleman.sureNo,
-                      eleman.sureAdi,
-                      eleman.deger,
-                      selectedSurahs,
-                      setSelectedSurahs
-                    );
-                  } else if (eleman.durum === "sureNo") {
-                    handleSurahNoClick(
-                      eleman.sureNo,
-                      eleman.sureAdi,
-                      setSelectedSurahs
-                    );
-                  } else {
-                    handleBasmalaClick(
-                      eleman.sureNo,
-                      eleman.sureAdi,
-                      true,
-                      selectedSurahs,
-                      setSelectedSurahs
-                    );
-                  }
+                  if (eleman.durum === "toplam-ayet-sayisi") handleTotalAyatClick(eleman.deger);
+                  else if (eleman.durum === "ayet-sayisi") handleTotalClick(eleman, selectedSurahs, setSelectedSurahs, orginQuranEmptyList);
+                  else if (eleman.durum === "sureNo") handleSurahNoClick(eleman, setSelectedSurahs);
+                  else if (eleman.durum === "besmele") handleBasmalaClick(eleman.sureNo, eleman.sureAdi, true, selectedSurahs, setSelectedSurahs);
+                  else handleAyatClick(eleman, selectedSurahs, setSelectedSurahs, orginQuranEmptyList);
                 }}
-                className={`${
-                  isDifferent(eleman, orginQuranEmptyList)
-                    ? "text-red-500 font-bold blink" // Eğer farklıysa kırmızı
-                    : eleman.durum === "ayet-sayisi"
-                    ? "text-yellow-400" // Ayet sayısı için sarı
-                    : eleman.durum === "besmele"
-                    ? "text-orange-400" // Ayet sayısı için sarı
-                    : eleman.durum === "ayetNo"
-                    ? "text-white" // Ayet numarası için beyaz
-                    : eleman.durum === "toplam-ayet-sayisi"
-                    ? "text-green-300" // Toplam ayet sayısı için mavi
-                    : eleman.durum === "sureNo"
-                    ? "text-blue-500" // sure numarası için mavi
-                    : ""
-                } ${
+                
+                className={`${isDiff ? "text-red-500 font-bold blink" :
+                  eleman.durum === "ayet-sayisi" ? "text-yellow-400" :
+                  eleman.durum === "besmele" ? "text-orange-400" :
+                  eleman.durum === "ayetNo" ? "text-white" :
+                  eleman.durum === "toplam-ayet-sayisi" ? "text-green-300" :
+                  eleman.durum === "sureNo" ? "text-blue-500" : ""} ${
                   selectedSurahs.includes(eleman.sureNo) ? "bg-green-700" : ""
                 } mr-1 mb-1 cursor-pointer`}
               >
@@ -209,8 +131,7 @@ const BesmeleEtkisi = () => {
           })}
         </SayiyiGosterenComponent>
       ) : (
-        //Sayının bilgi metnini gösteren bileşen
-        <BesmeleEtkisiInfo/>
+        <BesmeleEtkisiInfo />
       )}
     </KapsayiciComponent>
   );
