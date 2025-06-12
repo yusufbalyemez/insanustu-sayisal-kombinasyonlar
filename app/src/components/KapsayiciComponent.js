a7502344e5afe9aed30f52199a2efa3d313d7e9b
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2025-06-12 11:57:27 +0300

    Updates

--- app/src/components/KapsayiciComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const KapsayiciComponent = ({children}) => {
-  return (
-    <div className="flex flex-col justify-center items-center gap-2 mt-5 md:mt-10">{children}</div>
-  )
-}
-
-export default KapsayiciComponent
