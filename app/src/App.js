f7db7c49d46c5f8301e6a3c65c73495ad48e70a7
Yusuf Balyemez <yusuf.balyemez93@gmail.com>
2025-06-12 14:44:13 +0300

    bir önceki committeki hata düzeltildi

--- app/src/App.js
+++ app/src/App.js
@@ -4,7 +4,7 @@ import { QuranProvider } from "./context/quranListContext";
 import HomePage from "./pages/HomePage";
 import Number1 from "./pages/Number1";
 import Number2 from "./pages/Number2";
-import { Routes, Route, BrowserRouter , HashRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Number3 from "./pages/Number3";
 import { DifferentRefsProvider } from "./context/DifferentRefsContext";
@@ -24,46 +24,37 @@ import Number15 from "./pages/Number15";
 import { LanguageProvider } from "./context/LanguageContext";
 import { DifferenceProvider } from "./context/DifferenceContext";
 
-//Ortama Göre Uygun Router Seçme (Github da çalışması için gerekl)
-const isGithubPages = window.location.hostname.includes("github.io");
-const CustomRouter = isGithubPages ? HashRouter : BrowserRouter;
-
 function App() {
   return (
     <LanguageProvider>
-      <QuranProvider>
-        <DifferentRefsProvider>
-          <DifferenceProvider>
-            <CustomRouter>
-              <Navbar />
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route
-                  path="/insanustu-sayisal-kombinasyonlar"
-                  element={<HomePage />}
-                />
-                <Route path="/1" element={<Number1 />} />
-                <Route path="/2" element={<Number2 />} />
-                <Route path="/3" element={<Number3 />} />
-                <Route path="/4" element={<Number4 />} />
-                <Route path="/5" element={<Number5 />} />
-                <Route path="/6" element={<Number6 />} />
-                <Route path="/7" element={<Number7 />} />
-                <Route path="/8" element={<Number8 />} />
-                <Route path="/9" element={<Number9 />} />
-                <Route path="/10" element={<Number10 />} />
-                <Route path="/11" element={<Number11 />} />
-                <Route path="/12" element={<Number12 />} />
-                <Route path="/13" element={<Number13 />} />
-                <Route path="/14" element={<Number14 />} />
-                <Route path="/15" element={<Number15 />} />
-                <Route path="/basmala" element={<BesmeleEtkisi />} />
-              </Routes>
-              <ToastContainer autoClose={4000} position="top-center" />
-            </CustomRouter>
-          </DifferenceProvider>
-        </DifferentRefsProvider>
-      </QuranProvider>
+    <QuranProvider>
+      <DifferentRefsProvider>
+        <DifferenceProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/insanustu-sayisal-kombinasyonlar" element={<HomePage />} />
+            <Route path="/1" element={<Number1 />} />
+            <Route path="/2" element={<Number2 />} />
+            <Route path="/3" element={<Number3 />} />
+            <Route path="/4" element={<Number4 />} />
+            <Route path="/5" element={<Number5/>} />
+            <Route path="/6" element={<Number6/>} />
+            <Route path="/7" element={<Number7/>} />
+            <Route path="/8" element={<Number8/>} />
+            <Route path="/9" element={<Number9/>} />
+            <Route path="/10" element={<Number10/>} />
+            <Route path="/11" element={<Number11/>} />
+            <Route path="/12" element={<Number12/>} />
+            <Route path="/13" element={<Number13/>} />
+            <Route path="/14" element={<Number14/>} />
+            <Route path="/15" element={<Number15/>} />
+            <Route path="/basmala" element={<BesmeleEtkisi/>} />
+          </Routes>
+          <ToastContainer autoClose={4000} position="top-center" />
+        </DifferenceProvider>
+      </DifferentRefsProvider>
+    </QuranProvider>
     </LanguageProvider>
   );
 }
